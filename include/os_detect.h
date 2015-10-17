115e85076398834d2df2fd299eedf5d76c22caa7
Luke Yang <lukeyyanginit@gmail.com>
2015-10-17 05:10:42 -0700

    updated os detection header macros

--- include/os_detect.h
+++ include/os_detect.h
@@ -1,14 +1,32 @@
 #ifndef OS_DETECT_H
 #define OS_DETECT_H
 
-#ifdef __APPLE__
-  #include "TargetConditionals.h"
-  #if TARGET_OS_MAC
-    #define THIS_IS_OS_X
+#ifdef _WIN32
+  #define THIS_IS_WINDOWS_32
+  #ifdef _WIN64
+    #define THIS_IS_WINDOWS_64
   #endif
-#elif __linux
+#elif __APPLE__
+  #include "TargetConditionals.h"
+  #if TARGET_IPHONE_SIMULATOR
+    #define THIS_IS_IOS_SIMULATOR
+  #elif TARGET_OS_IPHONE
+    #define THIS_IS_IOS
+  #elif TARGET_OS_MAC
+    #define THIS_IS_OS_X
+  #else
+    #error "Unknown Apple OS"
+  #endif
+#elif __linux__
   #define THIS_IS_LINUX
+#elif __unix__
+  #define THIS_IS_UNIX
+#elif defined(_POSIX_VERSION)
+  #define THIS_IS_POSIX
+#else
+  #error "Unknown OS"
 #endif
+
 
 
 #endif
